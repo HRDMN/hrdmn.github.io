0a3e5d0225fbaf2d63212bcd3a0ac7699d013c21
HRDMN <hardman2048@gmail.com>
2024-08-29 01:20:50 +0500

    Update app.js

--- EKBINKBOT/app.js
+++ EKBINKBOT/app.js
@@ -82,7 +82,6 @@ Telegram.WebApp.onEvent("mainButtonClicked", function(){
 let usercard = document.getElementById("usercard");
 
 let p = document.createElement("p");
-p.innerText = `${tg.initDataUnsafe.user.first_name} 
-${tg.initDataUnsafe.user.last_name};
+p.innerText = `${tg.initDataUnsafe.user.first_name} ${tg.initDataUnsafe.user.last_name}`;
 
 usercard.appendChild(p);
